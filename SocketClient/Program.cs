443d679a0d5cb259d22e11648775a1f4a110a558
afornagieri <fornagieri.artur@gmail.com>
2022-03-31 19:46:44 -0300

    Client && Server

---
+++ SocketClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+            try
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3333);
+                socket.Connect(endPoint);
+
+                Console.WriteLine("Connected !");
+                Console.WriteLine("\n");
+
+                string info = Console.ReadLine();
+
+                byte[] infoSend = Encoding.Default.GetBytes(info);
+
+                socket.Send(infoSend, 0, infoSend.Length, SocketFlags.None);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error, connection closed.");
+            }
+
+            socket.Close();
+            Console.WriteLine("Press any key to exit...");
+           
+        }
+    }
+}
---
+++ SocketServer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3333);
+
+            socket.Bind(endPoint);
+
+            socket.Listen(5);
+
+            Console.WriteLine("Listening in 3333");
+
+            Socket listen = socket.Accept();
+
+            byte[] bytes = new byte[255];
+
+            int size = listen.Receive(bytes, 0, bytes.Length, SocketFlags.None);
+
+            Array.Resize(ref bytes, size);
+
+            Console.WriteLine("Someone speak: ");
+            Console.WriteLine(Encoding.Default.GetString(bytes));
+
+            socket.Close();
+
+            Console.WriteLine("Connection closed, press any key... \n");
+            Console.ReadKey();
+
+        }
+    }
+}
